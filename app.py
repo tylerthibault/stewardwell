43a5b2e7c2c78c739665024c1410197f67d7ecca
atbocompany <132010032+atbocompany@users.noreply.github.com>
2024-11-05 22:10:01 -0800

    removed the create_all() from app.py

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ from flask_app import app, db
 from flask_app.controllers import routes, user, family, budget, chores
 
 # Create tables
-with app.app_context():
-    db.create_all()
+# with app.app_context():
+#     db.create_all()
 
 # keep this at the bottom of this file!!
 if __name__=="__main__":	 
