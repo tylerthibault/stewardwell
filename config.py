3f5073ef9333b771706340df5e9f799fc157af62
atbocompany <132010032+atbocompany@users.noreply.github.com>
2024-11-09 13:51:45 -0800

    starting over again

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-key-please-change'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'data', 'app.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False 
